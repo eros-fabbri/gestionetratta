5e6312dc014e0b1afa654c8d27d212c91a84180c
eros fabbri <erosfabbridev@gmail.com>
2022-06-22 16:45:22 +0200

    repository base

---
+++ src/main/java/it/prova/gestionetratte/repository/tratta/TrattaRepository.java
@@ -0,0 +1,9 @@
+package it.prova.gestionetratte.repository.tratta;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.gestionetratte.model.Tratta;
+
+public interface TrattaRepository extends CrudRepository<Tratta, Long> {
+
+}
